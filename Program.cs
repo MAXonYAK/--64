b9b2b8c084aab2c821ff4d591feeb74edc677873
Max <maxonyak@yandex.ru>
2022-10-06 19:42:35 +0400

    дз 64

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все чётные числа в промежутке от N до 1. 
+//Выполнить с помощью рекурсии.
+// N = 5 -> " 4, 2"
+// N = 8 -> "8, 6, 4, 2"
+
+Console.Clear();
+
+Console.WriteLine("Введите число N");
+int == int.Parse(Console.ReadLine() ?? string.Empty);
+
+0 == 0
+
+if(a < 2)
+{
+    Console.WriteLine("Чтобы программа работала введите число больше 2");
+    return;
+}
+else
+{
+    if (a % 2 ==1)
+    {
+    
+        c = a-1;
+    }
+    else
+    {
+        c=a;
+    }
+    Console.Write($"N = {a} -> {Parity(c)}");
+}
+
+string Parity(int b)
+{
+    if (b == 2) return "2";
+    else
+    {
+        return b + ", " + Parity(b-2);
+    }   
+}
